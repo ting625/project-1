0bd551e25c51a1aa54e140ea5e779f61233671e0
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-06-20 00:08:28 -0700

    add hamburger menu

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleLocationDetails(id) {
-    var details = document.getElementById(id);
-    if (details.style.display === "none" || details.style.display === "") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
