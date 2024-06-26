396bd73a4881d1553b0a1945cf328ead7a0a701b
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-06-25 23:24:33 -0700

    add sign-up popup

--- modal.js
+++ modal.js
@@ -1,20 +1,22 @@
-function openModal() {
-    document.getElementById("modal").style.display = "block";
-  }
-  
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
-  
-  function toggleForm() {
-    const loginForm = document.getElementById("loginForm");
-    const signupForm = document.getElementById("signupForm");
-  
-    if (loginForm.style.display !== "none") {
-      loginForm.style.display = "none";
-      signupForm.style.display = "block";
-    } else {
-      loginForm.style.display = "block";
-      signupForm.style.display = "none";
+<!-- Custom Script -->
+
+    function openModal() {
+        $('#modal').modal('show');
     }
-  }
+
+    function closeModal() {
+        $('#modal').modal('hide');
+    }
+
+    function toggleForm() {
+        const loginForm = document.getElementById("loginForm");
+        const signupForm = document.getElementById("signupForm");
+
+        if (loginForm.style.display !== "none") {
+            loginForm.style.display = "none";
+            signupForm.style.display = "block";
+        } else {
+            loginForm.style.display = "block";
+            signupForm.style.display = "none";
+        }
+    }
